f81d35347e896a24bce1aece3be9da3c9c486fcd
kurotori <luissebastianangeles@gmail.com>
2015-12-06 00:45:45 -0300

    organización y nuevos archivos
    --
    Agregados los paquetes basico.comentado y basico.sinComentar para diferenciar los ejemplos con y sin comentarios

---
+++ basico/src/basico/sinComentar/MostrarTexto1.java
@@ -0,0 +1,22 @@
+//Ejemplo 1: Mostrar un texto en pantalla
+//---------------------------------------
+//Este ejemplo genera un ejecutable que
+//muestra en pantalla un texto pre-definido
+
+package basico;
+
+//La palabra clave 'package' indica la carpeta
+//donde se encuentra el archivo en el proyecto
+
+public class mostrarTexto1 {
+    
+    public static void main(String[] args) {
+//'public' se refiere a los permisos de acceso:
+// una clase, atributo o método 'public' es accesible
+// por completo por cualquier otro objeto
+//       
+        System.out.println("Esto es un ejemplo");
+        
+    }
+    
+}
